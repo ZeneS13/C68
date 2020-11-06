84c184561545da4ac202266026cd401b3de01f6f
ZeneS13 <sangoi.zene@gmail.com>
2020-11-06 16:59:17 +0530

    Modified

---
+++ screens/bookTransaction.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class TransactionScreen extends React.Component{
+render(){
+    return(
+        <Text>Issue or Return</Text>
+    )
+}
+
+
+
+}
---
+++ screens/searchScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class SearchScreen extends React.Component{
+render(){
+    return(
+        <Text style={{
+            marginTop:60,
+            marginLeft:50
+        }}>Search</Text>
+    )
+}
+
+
+
+}
